664220a395a40945182b2093fde0296b2a410a06
Aqua-Zhu <z891202z@gmail.com>
2024-12-03 12:05:02 +0800

    1203

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:process.env.NPDE_ENV === 'production' ?'/react-app1112/':'/',
+  base: process.env.NPDE_ENV === 'production' ?'/weather-text/':'/',
   plugins: [react()],
 })
